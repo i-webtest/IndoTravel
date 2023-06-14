d2cb7374b3d0878f290cbb1573cfcb0406e33a7b
i-webtest <stasbass80@gmail.com>
2023-06-14 21:44:15 +0300

    Added accordeon

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { accordeon } from './modules/accordeon.js';
 import { timer } from './modules/timer.js';
 
 const heroTimer = document.querySelector('.timer');
@@ -7,3 +8,4 @@ const deadline = heroTimer.getAttribute('data-timer-deadline');
 const timerDeadline = deadline.split('/').reverse();
 
 timer(timerDeadline);
+accordeon();
---
+++ js/modules/accordeon.js
@@ -0,0 +1,30 @@
+'use strict';
+
+export const accordeon = () => {
+  const items = document.querySelectorAll('.travel__item');
+  const buttons = document.querySelectorAll('.travel__item-title');
+  const textWrapper = document.querySelectorAll('.travel__item-text-wrapper');
+
+  let heightWrapper = 0;
+  //travel__item_active
+
+  textWrapper.forEach((elem) => {
+    if (heightWrapper < elem.scrollHeight) {
+      heightWrapper = elem.scrollHeight;
+    }
+  });
+
+  buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      for (let i = 0; i < items.length; i++) {
+        if (index === i) {
+          textWrapper[i].style.height = items[i].classList.contains('travel__item_active') ? '' : `${heightWrapper}px`;
+          items[i].classList.toggle('travel__item_active');
+        } else {
+          items[i].classList.remove('travel__item_active');
+          textWrapper[i].style.height = '';
+        }
+      }
+    });
+  });
+};

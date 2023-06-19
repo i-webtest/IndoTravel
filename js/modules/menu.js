080ca1cb274643057c14f502fe78e390d85ed869
i-webtest <stasbass80@gmail.com>
2023-06-19 22:56:36 +0300

    Added animation menu

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -5,6 +5,27 @@ export const menu = () => {
   const menu = document.querySelector('.header__menu');
   const menuLinks = document.querySelectorAll('.header__link');
 
+  menu.style.top = `-327px`;
+
+  const animate = ({ timing, draw, duration }) => {
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      }
+
+      let progress = timing(timeFraction);
+
+      draw(progress);
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  };
+
   const toggleClass = () => {
     menu.classList.toggle('header__menu_active');
   };
@@ -13,9 +34,21 @@ export const menu = () => {
     if (menu.classList.contains('header__menu_active')) {
       menu.classList.remove('header__menu_active');
     }
+    menu.style.top = `-327px`;
   };
 
-  burger.addEventListener('click', toggleClass);
+  burger.addEventListener('click', () => {
+    animate({
+      duration: 500,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        menu.style.top = 120 * progress + '%';
+      },
+    });
+    toggleClass();
+  });
 
   menuLinks.forEach((link) => {
     link.addEventListener('click', toggleClass);
